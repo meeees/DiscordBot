be239e2c9073306d67ee5e25b9401ebe814380a7
Michael <mcbarre12@gmail.com>
2019-07-12 19:05:22 -0500

    part 2

--- plugins/mtg_card_cmd.py
+++ plugins/mtg_card_cmd.py
@@ -13,14 +13,17 @@ async def mtgcard(message, args, author, client) :
 	if response.status_code == 404 :
 		await message.channel.send("Either card name was not found or too ambiguous")
 		return
-	await message.channel.send(response.json()['image_uris']['large'])
+	response = re.get(url)
+	res_json = response.json()
+	await message.channel.send(res_json['name'] + ': $'res_json['prices']['usd'] + '\n' + resresponse.json()['image_uris']['large'])
 
 
 if __name__ == '__main__' :
 	name = 'aust+com'
 	url = 'https://api.scryfall.com/cards/named?fuzzy=' + name
 	response = re.get(url)
-	print (response.json()['image_uris']['large'])
+	res_json = response.json()
+	print (res_json['image_uris']['large'] + '\n' + res_json['prices']['usd'])
 
 
 _command = bot_cmd("cardboard", mtgcard, 1, 'Search for a piece of cardboard by name')
