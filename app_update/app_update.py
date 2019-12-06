f998d97fb8def678381b13bf92633bd24372107d
Michael <mcbarre12@gmail.com>
2019-12-06 12:59:02 -0600

    more update things

--- app_update/app_update.py
+++ app_update/app_update.py
@@ -22,6 +22,7 @@ def get_webhook_post():
                 os.system(update_cmd)
             # or run an async function
             else :
+				os.system(update_cmd)
                 future = asyncio.run_coroutine_threadsafe(update_cmd(), update_coroutine_loop)
                 future.result()
 
